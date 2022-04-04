c1a448599d654e67d5fd956b69fcc9453d9f5e5f
viktor ojemeni <58849181+drex007@users.noreply.github.com>
2022-04-04 16:23:48 +0100

    removed staticfiles

--- pizza/settings.py
+++ pizza/settings.py
@@ -2,7 +2,7 @@
 
 from pathlib import Path
 from decouple import config
-
+import django_heroku
 import datetime
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -166,3 +166,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
