c475f3ab1179d27cc3628a1f77b49244100dfa28
viktor ojemeni <58849181+drex007@users.noreply.github.com>
2022-04-04 17:17:38 +0100

    changed static dir

--- pizza/settings.py
+++ pizza/settings.py
@@ -3,6 +3,7 @@
 from pathlib import Path
 from decouple import config
 import django_heroku
+import os
 import datetime
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -159,8 +160,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
